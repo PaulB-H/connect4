9511f60aabb67ccd2a482614010034461fba3c8e
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2022-10-06 03:59:18 -0400

    add drop function

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const board = [
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
 ];
+
+const drop = (color, col) => {
+  let freeSpace = false;
+  for (let i = 5; i >= 0; i--) {
+    if (!board[i][col]) {
+      board[i][col] = color;
+      freeSpace = true;
+      return board;
+    }
+  }
+  if (!freeSpace) {
+    return "No free space";
+  }
+};
